db48f83a6577eb1f147268f40fc3c6f34432e4e8
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-03 21:49:24 +0000

    updated task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
+    from add_0 import add as add_function
 
     a = 1
     b = 2
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add_function(a, b)
+    print("{} + {} = {}".format(a, b, result))
