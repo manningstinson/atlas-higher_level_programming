d0c61a57922d6e0f3b48bfd7b0c8e196f19bc3bf
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-03 21:59:33 +0000

    updated task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    num_args = len(argv) - 1
+
+    print("{} argument{}{}{}".format(
+        num_args,
+        "s" if num_args != 1 else "",
+        ":" if num_args > 0 else ".",
+        "\n" if num_args > 0 else ""
+    ))
+
+    for i, arg in enumerate(argv[1:], 1):
+        print("{}: {}".format(i, arg))
