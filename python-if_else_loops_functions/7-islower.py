34f4ba632281b6bda5837eced1293c4b31f1c6fe
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-03 20:49:54 +0000

    updated task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-def uppercase(input_str):
-
-
-    for char in input_str:
-        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print("{}".format(uppercase_char), end='')
-    print()
-
-uppercase("Hello, World!")
+def islower(c):
+    """Check if the character c is lowercase."""
+    return ord('a') <= ord(c) <= ord('z')
