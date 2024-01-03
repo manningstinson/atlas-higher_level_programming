8f301332ea65454df5db831bfcf194d757ae17a8
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-03 20:43:41 +0000

    updated task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -7,6 +7,3 @@ def uppercase(input_str):
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
         print("{}".format(uppercase_char), end='')
     print()
-
-
-uppercase("Hello, World!")
