c422bb108c3d88f45d9f1b7175c9e4a2c8e4433b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-03 23:21:53 +0000

    updated task 5

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import dis
+import types
+
+
+def print_names(module):
+    for name, value in module.__dict__.items():
+        if not name.startswith("__"):
+            print(name)
+
+
+if __name__ == "__main__":
+    with open("hidden_4.pyc", "rb") as file:
+        code_object = compile(file.read(), "hidden_4.pyc", "exec")
+        module = types.ModuleType("hidden_4")
+        exec(code_object, module.__dict__)
+
+    print_names(module)
