efa49f5c450af3f0e19650174d141ddd8e7cc713
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-04 19:52:44 +0000

    updating task 6

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+str_original = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str_modified = str_original[39:67] + str_original[107:112] + str_original[:6]
+print(str_modified)
