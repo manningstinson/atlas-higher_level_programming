d1619683c81036c86e0b0eeb1f90b39b6601e41a
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-04 19:05:09 +0000

    updated task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,16 +3,20 @@
 import sys
 
 
-def p():
-    n = len(sys.argv) - 1
-    if n == 0:
+def print_arguments():
+
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
         print("No arguments.")
     else:
-        s = 's' if n > 1 else ''
-        print("Number of arg{}: {}{}".format(s, n, '' if n == 1 else 's'))
-        for i, a in enumerate(sys.argv[1:], 1):
-            print("{}: {}".format(i, a))
+        plural_s = 's' if num_args > 1 else ''
+        print(f"Number of argument{plural_s}: {num_args}"
+              f"{'' if num_args == 1 else 's'}")
 
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
 
 if __name__ == "__main__":
-    p()
+    print_arguments()
+
