dcdaf5118977db0ec2cc03f801d712889e658046
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-05 21:06:43 +0000

    updated task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,7 +3,9 @@
 def print_list_integer(my_list=[]):
 
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
 
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+
+print_list_integer(my_list)
