c6d1f6650e7b7a32a0b39101640a68629e121ae2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-06 16:53:02 +0000

    updated task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=None):
+    # Check if the input list is None
+    if my_list is None:
+        print("Input list is None.")
+        return
+
     for num in reversed(my_list):
         print("{:d}".format(num))
-
-# Example usage:
