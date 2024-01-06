097a535e419dbe9ef3838ef7b94569e91fa9c768
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-06 16:47:38 +0000

    updated task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def replace_in_list(my_list, idx, element):
     # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
@@ -9,4 +8,3 @@ def replace_in_list(my_list, idx, element):
     # Replace the element at the specified index
     my_list[idx] = element
     return my_list
-
