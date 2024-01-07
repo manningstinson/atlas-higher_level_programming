b75cca9304f1ad65fb38ae975071b4a2065028e3
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 17:58:05 +0000

    updated task 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,10 +6,12 @@ def safe_print_list(my_list=[], x=0):
     elements_printed = 0
     for i in range(x):
         try:
-            print(my_list[i], end='')  # Print elements without spaces
+            # Print elements without spaces
+            print(my_list[i], end='')  
             elements_printed += 1
         except IndexError:
-            break  # Exit the loop when an IndexError occurs
-
-    print()  # Print a newline after all elements
+            # Exit the loop when an IndexError occurs
+            break  
+    # Print a newline after all elements
+    print()  
     return elements_printed
--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-
     try:
-        # Attempt to format as integer and print
-        print("{:d}".format(int(value)))
-        return True
-    except (ValueError, TypeError):
-        # Handle errors if value cannot be converted to integervi
-        return False
+        # Only print if conversion to integer is successful
+        if isinstance(int(value), int):
+            print("{:d}".format(int(value)))
+            return True
+        else:
+            return False
+        except (ValueError, TypeError):
+            # Handle errors if value cannot be converted to integer
+            return False
