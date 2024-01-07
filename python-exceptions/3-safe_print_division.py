7d7805fd08a07a95f77c9ee597c50ad25bd4a03b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 18:14:23 +0000

    updated task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("division by 0")
+        return None
+    except (TypeError, ValueError):
+        print("wrong type")
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
