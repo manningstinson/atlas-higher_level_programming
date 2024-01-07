eef0a5f21472e4ad04723c8d425c4131a427b2d8
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 17:52:22 +0000

    updated task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 
+def safe_print_integer(value):
 
-def safe_print_list(my_list=[], x=0):
-
-
-    # Initialize a variable to count the elements printed
-    elements_printed = 0
-
-    # Use try-except to handle potential IndexError
     try:
-
-        # Iterate through the list and print each element
-        for i in range(x):
-            print(my_list[i], end=' ')
-            elements_printed += 1
-    except IndexError:
-        # Catch the IndexError if x is greater than the length of my_li
-        pass
-
-    # Print a newline after the elements
-    print()
-
-    return elements_printed
+        # Attempt to format as integer and print
+        print("{:d}".format(int(value)))
+        return True
+    except (ValueError, TypeError):
+        # Handle errors if value cannot be converted to integervi
+        return False
