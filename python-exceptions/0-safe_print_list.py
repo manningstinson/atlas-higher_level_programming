57c52554b9f2340e6e7e117e93f740f0a0b79df2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 17:30:50 +0000

    updated task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,23 +2,14 @@
 
 
 def safe_print_list(my_list=[], x=0):
-
-
-    # Initialize a variable to count the elements printed
     elements_printed = 0
 
-    # Use try-except to handle potential IndexError
     try:
-
-        # Iterate through the list and print each element
         for i in range(x):
             print(my_list[i], end=' ')
             elements_printed += 1
     except IndexError:
-        # Catch the IndexError if x is greater than the length of my_li
         pass
 
-    # Print a newline after the elements
     print()
-
     return elements_printed
