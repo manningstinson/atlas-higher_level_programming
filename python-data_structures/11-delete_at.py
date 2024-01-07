0b8cc8ec9cd031093c9e082aef712493209d87e7
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 15:08:48 +0000

    updated task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
+
     # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
         return my_list
@@ -9,4 +10,3 @@ def delete_at(my_list=[], idx=0):
     new_list = my_list[:idx] + my_list[idx+1:]
 
     return new_list
-
