7da1f3c7c7c4964c06a82c62f2f3eaa7c9eab42f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 11:55:01 +0000

    updated task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,16 +2,15 @@
 
 
 def max_integer(my_list=[]):
-
     # Check if the list is empty
     if not my_list:
         return None
 
     # Initialize a variable to store the biggest integer
-    biggest_integer = my_list[0]
+    biggest_integer = my_list[0]  # Start with the first element
 
-    # Loop through the list and compare each element to the current biggest
-    for num in my_list:
+    # Loop through the list, starting from the second element
+    for num in my_list[1:]:  # Iterate from the second element onwards
         if num > biggest_integer:
             biggest_integer = num
 
