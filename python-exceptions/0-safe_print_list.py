a1380b0de5b51e352688b28a379242412cebe783
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 17:36:59 +0000

    updated task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,14 +2,14 @@
 
 
 def safe_print_list(my_list=[], x=0):
+
     elements_printed = 0
-
-    try:
-        for i in range(x):
-            print(my_list[i], end=' ')
+    for i in range(x):
+        try:
+            print(my_list[i], end='')  # Print elements without spaces
             elements_printed += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break  # Exit the loop when an IndexError occurs
 
-    print()
+    print()  # Print a newline after all elements
     return elements_printed
