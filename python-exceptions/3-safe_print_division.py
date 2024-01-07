f00345d4eca0acb04ee713a25eed5b5076c28cba
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 18:22:59 +0000

    updated

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-
     result = None  # Initialize result before the try block
     try:
         result = a / b
-        print(f"{a} / {b} = {result}")  # Print the result only if division is successful
     except ZeroDivisionError:
         print("division by 0")
     except (TypeError, ValueError):
         print("wrong type")
     finally:
-        print(f"Inside result: {result}")  # Always print the result, even if an error occurred
+        print("Inside result: {}".format(result))
         return result
 
