4a47405d6151d32eafaec42fa4264ecd45d08118
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 18:25:16 +0000

    updated

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -9,6 +9,9 @@ def safe_print_division(a, b):
     except (TypeError, ValueError):
         print("wrong type")
     finally:
-        print("Inside result: {}".format(result))
+        if result is not None:
+            print("Inside result: {}".format(result))
+        else:
+            print("Inside result: None")
         return result
 
