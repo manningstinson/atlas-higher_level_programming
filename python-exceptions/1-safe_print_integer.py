c0a4eee4a2567367d9983e94fa7ddf7030f7f35e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-07 18:01:40 +0000

    updated task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,12 +2,9 @@
 
 def safe_print_integer(value):
     try:
-        # Only print if conversion to integer is successful
-        if isinstance(int(value), int):
-            print("{:d}".format(int(value)))
-            return True
-        else:
-            return False
-        except (ValueError, TypeError):
-            # Handle errors if value cannot be converted to integer
-            return False
+        # Attempt to format as integer and print
+        print("{:d}".format(int(value)))
+        return True
+    except (ValueError, TypeError):
+        # Handle errors if value cannot be converted to integer
+        return False
