870310c4c012c503b53cfbe3702266f9caf1bd9e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-08 08:22:35 +0000

    updated task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
+    # If the key exists, update its value; otherwise, add a new key/value pair
     a_dictionary[key] = value
+    return a_dictionary
 
 # # Example Usage:
-# my_dict = {'banana': 3, 'apple': 5, 'orange': 2, 'grape': 7}
-# update_dictionary(my_dict, 'apple', 8)
-# update_dictionary(my_dict, 'kiwi', 4)
+# a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
+# new_dict = update_dictionary(a_dictionary, 'language', "Python")
 
-# print(my_dict)
+# # Printing the sorted dictionary
+# for key, value in sorted(new_dict.items()):
+#     print(f"{key}: {value}")
+
+# print("--")
+
+# # Printing the sorted original dictionary
+# for key, value in sorted(a_dictionary.items()):
+#     print(f"{key}: {value}")
