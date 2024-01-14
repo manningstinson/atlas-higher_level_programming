9880876fd9b79090ad9c100c4b67e53070b1a420
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-14 16:55:58 +0000

    updated task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,6 +10,7 @@ Usage:
     print(result)  # Output: 8
 """
 
+import math
 
 def add_integer(a, b=98):
     """
@@ -25,22 +26,16 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If a or b is not an integer or float.
-        ValueError: If a or b is a string, or if result is NaN.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
 
-    if isinstance(a, float) and (a.is_integer() or a.is_nan()):
+    if isinstance(a, float) and (a.is_integer() or math.isnan(a)):
         a = int(a)
 
-    if isinstance(b, float) and (b.is_integer() or b.is_nan()):
+    if isinstance(b, float) and (b.is_integer() or math.isnan(b)):
         b = int(b)
 
-    result = a + b
-
-    if isinstance(result, float) and result.is_integer():
-        return int(result)
-
-    raise ValueError("Result is NaN or not a valid integer.")
+    return int(a + b)
