67751278bac66336c91e0442edacbeb166250b58
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-14 23:25:59 +0000

    updated task 0

--- python-test_driven_development/tests/main.py
+++ python-test_driven_development/tests/main.py
@@ -1,28 +1,29 @@
-#!/usr/bin/python3
-from __import__('0-add_integer').add_integer
-from __import__('2-matrix_divided').matrix_divided
-from __import__('3-say_my_name').say_my_name
-from __import__('4-print_square').print_square
-from __import__('5-text_indentation').text_indentation
+# #!/usr/bin/python3
 
-# Test add_integer
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-# ... (similar calls for other functions)
+# from __import__('0-add_integer').add_integer
+# from __import__('2-matrix_divided').matrix_divided
+# from __import__('3-say_my_name').say_my_name
+# from __import__('4-print_square').print_square
+# from __import__('5-text_indentation').text_indentation
 
-# Test matrix_divided
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(matrix_divided(matrix, 3))
-# ... (similar calls for other functions)
+# # Test add_integer
+# print(add_integer(1, 2))
+# print(add_integer(100, -2))
+# # ... (similar calls for other functions)
 
-# Test say_my_name
-say_my_name("John", "Smith")
-# ... (similar calls for other functions)
+# # Test matrix_divided
+# matrix = [[1, 2, 3], [4, 5, 6]]
+# print(matrix_divided(matrix, 3))
+# # ... (similar calls for other functions)
 
-# Test print_square
-print_square(4)
-# ... (similar calls for other functions)
+# # Test say_my_name
+# say_my_name("John", "Smith")
+# # ... (similar calls for other functions)
+# a
+# # Test print_square
+# print_square(4)
+# # ... (similar calls for other functions)
 
-# Test text_indentation
-text_indentation("Lorem ipsum dolor...")
-# ... (similar calls for other functions)
+# # Test text_indentation
+# text_indentation("Lorem ipsum dolor...")
+# # ... (similar calls for other functions)
