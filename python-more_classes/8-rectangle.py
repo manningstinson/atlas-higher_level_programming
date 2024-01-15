dbb45ba2755a2ad4b7d4183bed4a601d1e7ff8e0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-15 09:36:42 +0000

    updated task 9

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -58,7 +58,8 @@ class Rectangle:
         """String representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return "\n".join([str(self.print_symbol) * self.__width] *
+                 self.__height)
 
     def __repr__(self):
         """Representation of the rectangle for recreation."""
