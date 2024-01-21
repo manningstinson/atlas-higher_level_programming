661c67f38ba49e1e959de74139003631fe66dd1e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-21 16:28:33 +0000

    updated task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3
 """
-Module 10-square.py
-Defines a Square class that inherits from Rectangle.
+Module containing the Square class.
 """
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    Represents a square.
+    Square class that inherits from Rectangle.
     """
 
     def __init__(self, size):
         """
-        Initializes a new instance of the Square class.
-
-        Parameters:
-        - size (int): The size of the square.
+        Initializes a Square instance.
+        Args:
+            size (int): The size of the square.
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """
-        Computes the area of the square.
-
+        Calculates the area of the square.
         Returns:
-        - The area of the square.
+            int: The area of the square.
         """
         return self.__size ** 2
 
     def __str__(self):
         """
         Returns a string representation of the square.
-
-        Format: [Square] <width>/<height>
-
         Returns:
-        - String representation of the square.
+            str: String representation of the square.
         """
         return "[Square] {}/{}".format(self.__size, self.__size)
+
+
+if __name__ == "__main__":
+    s = Square(13)
+    print(s)
+    print(s.area())
