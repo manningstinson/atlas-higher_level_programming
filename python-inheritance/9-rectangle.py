2ffbebab9cef2439c4a9b2bec853a57613bb8e37
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-21 16:15:06 +0000

    updated task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,49 +1,41 @@
 #!/usr/bin/python3
 """
-Module 9-rectangle
-Defines a Rectangle class that inherits from BaseGeometry.
+Module 7-base_geometry
+Defines a BaseGeometry class.
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class Rectangle(BaseGeometry):
+class BaseGeometry:
     """
-    A class representing a rectangle.
+    A class representing a base geometry.
     """
-
-    def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        super().__init__()
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
 
     def area(self):
         """
-        Computes the area of the rectangle.
+        Computes the area of the geometry.
+
+        Raises:
+            Exception: If area is not implemented.
 
         Returns:
-            int: The area of the rectangle.
+            int: The area of the geometry.
         """
-        return self.__width * self.__height
+        raise Exception("area() is not implemented")
 
-    def __str__(self):
+    def integer_validator(self, name, value):
         """
-        Returns a string representation of the rectangle.
+        Validates the value.
 
-        Returns:
-            str: The string representation of the rectangle.
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
         """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-# # Test the implementation
-# if __name__ == "__main__":
-#     r = Rectangle(3, 5)
-#     print(r)
-#     print(r.area())
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
