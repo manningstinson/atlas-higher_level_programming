3fe7348b3ef3d3b201a3f7cb75c19e8bc300a58d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-22 22:26:33 +0000

    updated task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-"""Module for reading and printing the contents of a text file."""
+"""
+Module for reading and printing the content of a text file.
+
+This module defines a function read_file that reads the content
+of a specified text file (UTF8) and prints it to stdout.
+
+Functions:
+    read_file(filename=""): Read and print the content of a text file.
+
+Usage:
+    Example usage is provided at the bottom of the script.
+
+"""
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout.
+    """
+    Read and print the content of a text file (UTF8).
 
     Args:
-        filename (str): The name of the text file to be read.
+        filename (str): The name of the file to read.
 
     Returns:
         None
     """
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         content = file.read()
         print(content)
 
 
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+# # Example usage:
+# if __name__ == "__main__":
+#     read_file("my_file_0.txt")
