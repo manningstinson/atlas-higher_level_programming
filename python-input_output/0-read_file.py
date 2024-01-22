4f9c08b25940d73885c6bede89a67d19b7943e43
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-22 22:55:35 +0000

    updated task 8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -13,7 +13,6 @@ Usage:
 
 """
 
-
 def read_file(filename=""):
     """
     Read and print the content of a text file (UTF8).
@@ -24,11 +23,28 @@ def read_file(filename=""):
     Returns:
         None
     """
-    with open(filename, encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+    try:
+        with open(filename, encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
+# Example usage:
+if __name__ == "__main__":
+    # Test Case: Correct output - case: 1 line
+    read_file("my_file_0.txt")
 
-# # Example usage:
-# if __name__ == "__main__":
-#     read_file("my_file_0.txt")
+    # Test Case: Correct output - case: not found
+    read_file("nonexistent_file.txt")
+
+    # Test Case: Correct output - case: empty
+    read_file("empty_file.txt")
+
+    # Test Case: Correct output - case: big HTML text
+    read_file("big_html_file.txt")
+
+    # Test Case: Correct output - case: 5 paragraphs
+    read_file("five_paragraphs.txt")
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module for class to JSON serialization
+"""
+
+
+def class_to_json(obj):
+    """ Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary containing serializable attributes of the object.
+
+    """
+
+    # Get the dictionary representation of the object's attributes
+    obj_dict = obj.__dict__
+
+    # Convert any nested objects to dictionaries recursively
+    for key, value in obj_dict.items():
+        if hasattr(value, '__dict__'):
+            obj_dict[key] = class_to_json(value)
+
+    return obj_dict
+
+
+if __name__ == "__main__":
+    # Test cases
+    MyClass = __import__('8-my_class').MyClass
+    m = MyClass("John")
+    m.number = 89
+    print(type(m))
+    print(m)
+
+    mj = class_to_json(m)
+    print(type(mj))
+    print(mj)
