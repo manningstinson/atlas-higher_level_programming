339ec52d1e76e80202742ca40296907d28636604
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-26 17:42:33 +0000

    added project - python-almost-a-circle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,8 @@ Module for writing to a file.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
 
     Args:
         filename (str): The name of the file.
