7ee4c2d968ca198969b3ff671fffae9a06f1f740
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-27 21:46:40 +0000

    updated task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,10 @@ class Rectangle(Base):
         super().__init__(id)
 
         # Private instance attributes with public getter and setter
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -27,6 +27,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Setter for width attribute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -37,6 +41,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ Setter for height attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -47,6 +55,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Setter for x attribute """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -57,4 +69,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Setter for y attribute """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
