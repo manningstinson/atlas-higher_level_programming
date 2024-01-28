79e8c5c1f7374f0d4cd7c48a75d124d3e2bf44f5
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 09:29:09 +0000

    updated base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,25 @@
+#!/usr/bin/python3
+"""
+Base module
+"""
+
+
 import json
 
+
 class Base:
-    """ Base class """
+    """
+    Base class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Constructor method """
+        """
+        Constructor method
+
+        Args:
+            id (int): object identifier
+        """
         if id is not None:
             self.id = id
         else:
@@ -14,7 +28,15 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Convert list of dictionaries to JSON string """
+        """
+        Convert list of dictionaries to JSON string
+
+        Args:
+            list_dictionaries (list): List of dictionaries
+
+        Returns:
+            str: JSON representation of list_dictionaries
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
