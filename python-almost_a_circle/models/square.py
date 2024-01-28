eb1c9efc81dc548b376022117a69e90985a98ba1
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 08:10:01 +0000

    updated square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,7 +1,8 @@
-# models/square.py
 #!/usr/bin/python3
+
 """Module that defines the Square class"""
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """Square class, inherits from Rectangle"""
@@ -12,13 +13,15 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return a string representation of the Square"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format
+        (self.id, self.x, self.y, self.width)
 
     def area(self):
         """Return the area of the Square"""
         return self.width * self.height
 
     # Additional methods or overrides can be added here if needed
+
 
 if __name__ == "__main__":
     # Example usage as described in the prompt
