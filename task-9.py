b259147a80c6d038fdae3bf9597b65b41450911e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 07:53:06 +0000

    updated bkup

---
+++ task-9.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+""" Rectangle Module """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Class Rectangle inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+
+        # Call the super class with id
+        super().__init__(id)
+
+        # Private instance attributes with public getter and setter
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width attribute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height attribute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x attribute """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x attribute """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y attribute """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y attribute """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def __str__(self):
+        """ String representation of the object """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    def display(self):
+        """ Display the rectangle using '#' """
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def update(self, *args, **kwargs):
+        """ Update the attributes of the rectangle """
+
+        # Define the order of attributes and their corresponding names
+        attr_names = ["id", "width", "height", "x", "y"]
+
+        # Update attributes using positional arguments
+        for i, arg in enumerate(args):
+            setattr(self, attr_names[i], arg)
+
+        # Update attributes using keyword arguments
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+if __name__ == "__main__":
+    r1 = Rectangle(10, 10, 10, 10)
+    print(r1)
+
+    r1.update(height=1)
+    print(r1)
+
+    r1.update(width=1, x=2)
+    print(r1)
+
+    r1.update(y=1, width=2, x=3, id=89)
+    print(r1)
+
+    r1.update(x=1, height=2, y=3, width=4)
+    print(r1)
