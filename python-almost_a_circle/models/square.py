0e16416f569a17f89ac3006edef88526db383520
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 09:18:39 +0000

    updated square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -49,5 +49,18 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """ Returns the dictionary representation of a Square """
-        return
-        {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+
+
+if __name__ == "__main__":
+    s1 = Square(10, 2, 1)
+    print(s1)
+    s1_dictionary = s1.to_dictionary()
+    print(s1_dictionary)
+    print(type(s1_dictionary))
+
+    s2 = Square(1, 1)
+    print(s2)
+    s2.update(**s1_dictionary)
+    print(s2)
+    print(s1 == s2)
