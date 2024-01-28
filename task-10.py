f5506e54194b343a1c361af77ceff9a16f9dcd7b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 09:02:02 +0000

    updated bkup

--- task-10.py
+++ task-10.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
+""" Square Module """
 
-"""Module that defines the Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class, inherits from Rectangle"""
+    """ Class Square inherits from Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a Square instance"""
+        """ Constructor method """
+
+        # Call the super class with id, x, y, width, and height
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Return a string representation of the Square"""
-        return "[Square] ({}) {}/{} - {}".format
-        (self.id, self.x, self.y, self.width)
+    @property
+    def size(self):
+        """ Getter for size attribute """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size attribute """
+        self.width = value
+        self.height = value
 
     def area(self):
-        """Return the area of the Square"""
+        """ Returns the area of the square """
         return self.width * self.height
 
-    # Additional methods or overrides can be added here if needed
+    def __str__(self):
+        """ String representation of the object """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
+    def update(self, *args, **kwargs):
+        """ Update the attributes of the square """
 
-# if __name__ == "__main__":
-#     # Example usage as described in the prompt
-#     s1 = Square(2)
-#     print(s1)
-#     if str(s1) != "[Square] (1) 0/0 - 2":
-#         print("Error in __str__ method for Square class")
+        # Define the order of attributes and their corresponding names
+        attr_names = ["id", "size", "x", "y"]
+
+        # Update attributes using positional arguments
+        for i, arg in enumerate(args):
+            setattr(self, attr_names[i], arg)
+
+        # Update attributes using keyword arguments
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Square """
+        return
+        {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
