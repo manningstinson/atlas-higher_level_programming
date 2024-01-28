9a6262f202a89d9f9a2d655964394ed768a6af1c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-28 09:35:35 +0000

    updated bkup

---
+++ task-16.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Base module
+"""
+
+
+import json
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor method
+
+        Args:
+            id (int): object identifier
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Convert list of dictionaries to JSON string
+
+        Args:
+            list_dictionaries (list): List of dictionaries
+
+        Returns:
+            str: JSON representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save JSON representation of list_objs to a file
+
+        Args:
+            list_objs (list): List of instances inheriting from Base
+        """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(json_list))
