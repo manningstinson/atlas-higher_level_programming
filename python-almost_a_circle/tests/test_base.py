7e5f3d32840ff9bad2d721a0090c04ba94006f1c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-01-30 13:30:29 -0600

    Update test_base.py

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,55 +1,49 @@
-
-import sys
-sys.path.append("/atlas-higher_level_programming/python-almost_a_circle/models")
-
 import unittest
-from models.base import Base
+from your_module_name import Base, Rectangle
 
+class TestBaseClass(unittest.TestCase):
 
-class TestBase(unittest.TestCase):
-
-    def test_constructor(self):
+    def test_auto_assign_id(self):
         obj1 = Base()
         obj2 = Base()
-        self.assertIsInstance(obj1, Base)
-        self.assertIsInstance(obj2, Base)
-        self.assertNotEqual(obj1.id, obj2.id)
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
 
-    def test_to_json_string(self):
-        obj = Base()
-        json_string = Base.to_json_string([obj.to_dictionary()])
-        self.assertEqual(json_string, '[{"id": 1}]')
+    def test_auto_assign_incremental_id(self):
+        obj1 = Base()
+        obj2 = Base()
+        obj3 = Base()
+        self.assertEqual(obj3.id, obj2.id + 1)
 
-    def test_save_to_file(self):
-        obj = Base()
-        Base.save_to_file([obj])
-        with open("Base.json", "r") as file:
-            content = file.read()
-            self.assertEqual(content, '[{"id": 1}]')
+    def test_specified_id(self):
+        obj = Base(89)
+        self.assertEqual(obj.id, 89)
 
-    def test_from_json_string(self):
-        json_string = '[{"id": 1}]'
-        obj_list = Base.from_json_string(json_string)
-        self.assertEqual(obj_list, [{'id': 1}])
+    def test_to_json_string_none(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
 
-    def test_create(self):
-        obj = Base.create(**{'id': 1, 'width': 2, 'height': 3})
-        self.assertIsInstance(obj, Base)
-        self.assertEqual(obj.id, 1)
-        self.assertEqual(obj.width, 2)
-        self.assertEqual(obj.height, 3)
+    def test_to_json_string_empty_list(self):
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
 
-    def test_update(self):
-        obj = Base()
-        obj.update(id=2, width=5, height=7)
-        self.assertEqual(obj.id, 2)
-        self.assertEqual(obj.width, 5)
-        self.assertEqual(obj.height, 7)
+    def test_to_json_string_with_data(self):
+        data = [{'id': 12}]
+        result = Base.to_json_string(data)
+        self.assertEqual(result, '[{"id": 12}]')
 
-    def test_to_dictionary(self):
-        obj = Base(**{'id': 1, 'width': 2, 'height': 3})
-        dictionary = obj.to_dictionary()
-        self.assertEqual(dictionary, {'id': 1, 'width': 2, 'height': 3})
+    def test_from_json_string_none(self):
+        result = Base.from_json_string(None)
+        self.assertEqual(result, [])
+
+    def test_from_json_string_empty_string(self):
+        result = Base.from_json_string("[]")
+        self.assertEqual(result, [])
+
+    def test_from_json_string_with_data(self):
+        json_str = '[{"id": 89}]'
+        result = Base.from_json_string(json_str)
+        self.assertEqual(result, [{'id': 89}])
 
 if __name__ == '__main__':
     unittest.main()
