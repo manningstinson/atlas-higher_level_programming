be2ec9816067c059c943e6ee746993ca2c539548
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-19 23:31:07 +0000

    updated task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
--- This query selects the title of TV shows and their associated genre IDs from the tables tv_shows and tv_show_genres.
--- It joins the tables based on the relationship between tv_shows.id and tv_show_genres.tv_show_id.
--- Results are ordered alphabetically by the title of the TV show and then by the genre ID.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
