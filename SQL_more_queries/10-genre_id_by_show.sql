6338f8777ff64f7f8b963e0251961533eba3f968
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-19 23:25:50 +0000

    updated task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,4 @@
+-- This query selects the title of TV shows and their associated genre IDs from two tables: tv_shows and tv_show_genres.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
