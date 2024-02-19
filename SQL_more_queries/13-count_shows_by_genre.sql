f6f72e5639ad08163cd198ca8b934cd1e056aa79
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-02-19 23:41:39 +0000

    updated task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,16 +1,10 @@
--- Script: 13-count_shows_by_genre.sql
--- Description: This script lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
--- Each record displays the TV show genre and the number of shows linked to that genre.
--- Genres without any linked shows are not displayed.
--- Results are sorted in descending order by the number of shows linked.
--- Usage: Execute this script with the database name passed as an argument of the mysql command.
--- Example: cat 13-count_shows_by_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
--- Author: [Your Name]
--- Date: [Date]
--- Version: 1.0
-
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM tv_show_genres
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC;
+-- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- shows linked to each. Does not display genres without linked shows. Records
+-- are ordered by descending number of shows linked.
+SELECT g.name AS genre,
+       COUNT(*) AS number_of_shows
+  FROM tv_genres AS g
+       INNER JOIN tv_show_genres AS t
+       ON g.id = t.genre_id
+ GROUP BY g.name
+ ORDER BY number_of_shows DESC;
