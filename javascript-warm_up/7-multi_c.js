365abb8325172cb85075b15b609ff32b7c8d11c9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-21 22:07:47 +0000

    updated task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x) || x < 1) {
-  console.log('Missing number of occurrences');
+  // Do nothing when input is invalid
 } else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
