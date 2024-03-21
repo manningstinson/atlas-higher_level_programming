f1bdd85170e53781933a5af27b10e7053071f95a
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-21 22:36:40 +0000

    updated task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,12 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {}; // Return an empty object if width or height is not a positive integer
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
