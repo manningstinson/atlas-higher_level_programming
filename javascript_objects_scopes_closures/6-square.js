d80e81d64ca7a56e90736dd084c4e6c9f08be1e4
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-22 08:08:31 +0000

    updated task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    // If c is undefined, use 'X' as default character
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    // Print the square using the character c
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
