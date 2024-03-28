51b607f1efedc24acecdadd263f382f8c9fb2b40
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-28 22:32:07 +0000

    updated task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 
-const request = require("request");
-
+const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request.get(apiUrl, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	if (response.statusCode !== 200) {
-		console.error(
-			`Failed to fetch movie details. Status code: ${response.statusCode}`
-		);
-		return;
-	}
+  if (response.statusCode !== 200) {
+    console.error(
+      `Failed to fetch movie details. Status code: ${response.statusCode}`
+    );
+    return;
+  }
 
-	const movieData = JSON.parse(body);
-	console.log(movieData.title);
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
 });
