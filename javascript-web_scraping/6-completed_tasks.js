59bf44b6e1115e146295bf44710cb60c940489f3
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-28 23:12:13 +0000

    updated task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-	if (error) {
-		console.error("Error:", error);
-		return;
-	}
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
 
-	if (response.statusCode !== 200) {
-		console.error("Status:", response.statusCode);
-		return;
-	}
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
 
-	const todos = JSON.parse(body);
-	const completedTasks = {};
+  const todos = JSON.parse(body);
+  const completedTasks = {};
 
-	todos.forEach((todo) => {
-		if (todo.completed) {
-			if (completedTasks[todo.userId]) {
-				completedTasks[todo.userId]++;
-			} else {
-				completedTasks[todo.userId] = 1;
-			}
-		}
-	});
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
 
-	console.log(completedTasks);
+  console.log(completedTasks);
 });
