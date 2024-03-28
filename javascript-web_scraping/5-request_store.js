eab09e75e2f7b7633b19cb6cf99e6b5e107aaa88
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-28 22:52:10 +0000

    updated task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(
+      `Failed to fetch webpage. Status code: ${response.statusCode}`
+    );
+    return;
+  }
+
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('Webpage content has been successfully stored in:', filePath);
+    }
+  });
+});
