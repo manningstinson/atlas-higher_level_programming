83e5df85624cf191f4e7185a181cfad00e4c8d35
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-28 23:06:36 +0000

    udpated task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
