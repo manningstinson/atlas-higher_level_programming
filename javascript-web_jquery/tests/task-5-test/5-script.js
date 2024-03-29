73dceb045ef1c1875f55b2332e27a0d73baff1ef
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-29 14:12:01 +0000

    updated task 5

---
+++ javascript-web_jquery/tests/task-5-test/5-script.js
@@ -0,0 +1,11 @@
+/* global $ */
+
+$(document).ready(function () {
+	// Attach click event listener to DIV#add_item
+	$("#add_item").click(function () {
+		// Create a new <li> element
+		const newItem = $("<li>Item</li>");
+		// Append the new <li> element to UL.my_list
+		$("ul.my_list").append(newItem);
+	});
+});
