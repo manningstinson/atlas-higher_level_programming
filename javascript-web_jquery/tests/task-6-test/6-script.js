af1a7fd011f2b5cc694a83bb5697fed9aa4c224a
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-29 14:22:47 +0000

    updated task 6

---
+++ javascript-web_jquery/tests/task-6-test/6-script.js
@@ -0,0 +1,9 @@
+/* global $ */
+
+$(document).ready(function () {
+	// Attach click event listener to DIV#update_header
+	$("#update_header").click(function () {
+		// Update the text of the <header> element
+		$("header").text("New Header!!!");
+	});
+});
