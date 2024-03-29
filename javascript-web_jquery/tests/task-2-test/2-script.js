0e83a94166da5cd01143837c4fbe1fda6e8b2f14
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-29 13:51:43 +0000

    updated task 2

---
+++ javascript-web_jquery/tests/task-2-test/2-script.js
@@ -0,0 +1,9 @@
+const jQuery = window.jQuery;
+
+jQuery(document).ready(function () {
+	// Attach click event listener to DIV#red_header
+	jQuery("div#red_header").click(function () {
+		// Update text color of the <header> element to red
+		jQuery("header").css("color", "#FF0000");
+	});
+});
