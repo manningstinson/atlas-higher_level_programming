b48d73649977d9c00dadad7a7931f364452c9cde
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-03-29 14:42:38 +0000

    updated task 8

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+/* global $ */
+
+$(document).ready(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    data.results.forEach(function (movie) {
+      $('<li>').text(movie.title).appendTo('#list_movies');
+    });
+  });
+});
